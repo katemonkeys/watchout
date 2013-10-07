3caa92943bce374045693cbc54dd3bb40582c26a
Kate Jenkins <catherine.ann.jenkins@gmail.com>
2013-10-06 20:22:23 -0700

    Fixing indentation

--- ourCollider.js
+++ ourCollider.js
@@ -1,5 +1,5 @@
 // (function() {
-var axes, enemyData,gameBoard, gameOptions,gameStats,play,render,Player;
+var axes, enemyData,gameBoard, gameOptions,gameStats,play,render,Player,updateScore,updateBestScore;
 var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
 gameOptions = {
@@ -21,117 +21,113 @@ axes = {
 
 gameBoard = d3.select('.container').append('svg:svg').attr('width', gameOptions.width).attr('height', gameOptions.height);
 
-  var updateScore = function() {
-    return d3.select('#current-score').text(gameStats.score.toString());
+updateScore = function() {
+  return d3.select('#current-score').text(gameStats.score.toString());
+};
+
+updateBestScore = function() {
+  gameStats.bestScore = _.max([gameStats.bestScore, gameStats.score]);
+  return d3.select('#best-score').text(gameStats.bestScore.toString());
+};
+
+Player = (function() {
+
+  function Player(gameOptions) {
+    this.setupDragging = __bind(this.setupDragging, this);
+    this.moveRelative = __bind(this.moveRelative, this);
+    this.moveAbsolute = __bind(this.moveAbsolute, this);
+    this.transform = __bind(this.transform, this);
+    this.setY = __bind(this.setY, this);
+    this.getY = __bind(this.getY, this);
+    this.setX = __bind(this.setX, this);
+    this.getX = __bind(this.getX, this);
+    this.render = __bind(this.render, this);
+    this.gameOptions = gameOptions;
+  }
+
+  Player.prototype.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+
+  Player.prototype.fill = '#0000ff';
+
+  Player.prototype.x = 0;
+
+  Player.prototype.y = 0;
+
+  Player.prototype.angle = 0;
+
+  Player.prototype.r = 5;
+
+  Player.prototype.render = function(to) {
+    this.el = to.append('svg:path').attr('d', this.path).attr('fill', this.fill);
+    this.transform({
+      x: this.gameOptions.width * 0.5,
+      y: this.gameOptions.height * 0.5
+    });
+    this.setupDragging();
+    return this;
   };
 
-  var updateBestScore = function() {
-    gameStats.bestScore = _.max([gameStats.bestScore, gameStats.score]);
-    return d3.select('#best-score').text(gameStats.bestScore.toString());
+  Player.prototype.getX = function() {
+    return this.x;
   };
 
- Player = (function() {
+  Player.prototype.setX = function(x) {
+    var maxX, minX;
+    minX = this.gameOptions.padding;
+    maxX = this.gameOptions.width - this.gameOptions.padding;
+    return this.x = x;
+  };
 
-    function Player(gameOptions) {
-      this.setupDragging = __bind(this.setupDragging, this);
-      this.moveRelative = __bind(this.moveRelative, this);
-      this.moveAbsolute = __bind(this.moveAbsolute, this);
-      this.transform = __bind(this.transform, this);
-      this.setY = __bind(this.setY, this);
-      this.getY = __bind(this.getY, this);
-      this.setX = __bind(this.setX, this);
-      this.getX = __bind(this.getX, this);
-      this.render = __bind(this.render, this);
-      this.gameOptions = gameOptions;
-    }
+  Player.prototype.getY = function() {
+    return this.y;
+  };
 
-    Player.prototype.path = 'm-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z';
+  Player.prototype.setY = function(y) {
+    var maxY, minY;
+    minY = this.gameOptions.padding;
+    maxY = this.gameOptions.height - this.gameOptions.padding;
+    return this.y = y;
+  };
 
-    Player.prototype.fill = '#0000ff';
+  Player.prototype.transform = function(opts) {
+    this.angle = opts.angle || this.angle;
+    this.setX(opts.x || this.x);
+    this.setY(opts.y || this.y);
+    return this.el.attr('transform', ("rotate(" + this.angle + "," + (this.getX()) + "," + (this.getY()) + ") ") + ("translate(" + (this.getX()) + "," + (this.getY()) + ")"));
+  };
 
-    Player.prototype.x = 0;
+  Player.prototype.moveAbsolute = function(x, y) {
+    return this.transform({
+      x: x,
+      y: y
+    });
+  };
 
-    Player.prototype.y = 0;
+  Player.prototype.moveRelative = function(dx, dy) {
+    return this.transform({
+      x: this.getX() + dx,
+      y: this.getY() + dy,
+      angle: 360 * (Math.atan2(dy, dx) / (Math.PI * 2))
+    });
+  };
 
-    Player.prototype.angle = 0;
-
-    Player.prototype.r = 5;
-
-
-    Player.prototype.render = function(to) {
-      this.el = to.append('svg:path').attr('d', this.path).attr('fill', this.fill);
-      this.transform({
-        x: this.gameOptions.width * 0.5,
-        y: this.gameOptions.height * 0.5
-      });
-      this.setupDragging();
-      return this;
+  Player.prototype.setupDragging = function() {
+    var drag, dragMove,
+      _this = this;
+    dragMove = function() {
+      return _this.moveRelative(d3.event.dx, d3.event.dy);
     };
+    drag = d3.behavior.drag().on('drag', dragMove);
+    return this.el.call(drag);
+  };
 
-    Player.prototype.getX = function() {
-      return this.x;
-    };
+  return Player;
 
-    Player.prototype.setX = function(x) {
-      var maxX, minX;
-      minX = this.gameOptions.padding;
-      maxX = this.gameOptions.width - this.gameOptions.padding;
-      return this.x = x;
-    };
+})();
 
-    Player.prototype.getY = function() {
-      return this.y;
-    };
+players = [];
 
-    Player.prototype.setY = function(y) {
-      var maxY, minY;
-      minY = this.gameOptions.padding;
-      maxY = this.gameOptions.height - this.gameOptions.padding;
-      return this.y = y;
-    };
-
-    Player.prototype.transform = function(opts) {
-      this.angle = opts.angle || this.angle;
-      this.setX(opts.x || this.x);
-      this.setY(opts.y || this.y);
-      return this.el.attr('transform', ("rotate(" + this.angle + "," + (this.getX()) + "," + (this.getY()) + ") ") + ("translate(" + (this.getX()) + "," + (this.getY()) + ")"));
-    };
-
-    Player.prototype.moveAbsolute = function(x, y) {
-      return this.transform({
-        x: x,
-        y: y
-      });
-    };
-
-    Player.prototype.moveRelative = function(dx, dy) {
-      return this.transform({
-        x: this.getX() + dx,
-        y: this.getY() + dy,
-        angle: 360 * (Math.atan2(dy, dx) / (Math.PI * 2))
-      });
-    };
-
-    Player.prototype.setupDragging = function() {
-      var drag, dragMove,
-        _this = this;
-      dragMove = function() {
-        return _this.moveRelative(d3.event.dx, d3.event.dy);
-      };
-      drag = d3.behavior.drag().on('drag', dragMove);
-      return this.el.call(drag);
-    };
-
-    return Player;
-
-  })();
-
-  players = [];
-
-  players.push(new Player(gameOptions).render(gameBoard));
-
-
-//var player = new Player(gameOptions).render(gameBoard);
+players.push(new Player(gameOptions).render(gameBoard));
 
 enemyData = function() {
   return _.range(0,gameOptions.nEnemies).map(function(i){
@@ -145,11 +141,11 @@ enemyData = function() {
 
 
 render = function(enemy_data) {
-  var checkCollision, enemies, onCollision, tweenWithCollisionDetection;
+  var checkCollision, enemies, onCollision, tweenWithCollisionDetection, svgElement;
 
   enemies = gameBoard.selectAll('circle').data(enemy_data, function(d) {return d.id;});
 
-  var svgElement = d3.select('.container').select('svg');
+  svgElement = d3.select('.container').select('svg');
   enemies.enter().append('circle').attr("cx",function(enemyData){return axes.x(enemyData.x);})
     .attr("cy",function(enemyData){return axes.y(enemyData.y);}).attr("r",10);
 
@@ -210,8 +206,3 @@ play = function() {
 };
 
 play();
-
-// }).call(this);
-
-
-
